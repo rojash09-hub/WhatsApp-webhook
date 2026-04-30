b5c94f37ffa0a03787a8822d2c7d2592609c3eb7
rojash09-hub <rojash09@gmail.com>
2026-04-29 19:32:44 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+const VERIFY_TOKEN = "mi_token_123";
+
+app.use(express.json());
+
+app.get('/webhook', (req, res) => {
+  const mode = req.query['hub.mode'];
+  const token = req.query['hub.verify_token'];
+  const challenge = req.query['hub.challenge'];
+
+  if (mode && token === VERIFY_TOKEN) {
+    res.status(200).send(challenge);
+  } else {
+    res.sendStatus(403);
+  }
+});
+
+app.post('/webhook', (req, res) => {
+  console.log("Mensaje recibido:");
+  console.log(JSON.stringify(req.body, null, 2));
+  res.sendStatus(200);
+});
+
+app.listen(PORT, () => {
+  console.log("Servidor corriendo");
+});
