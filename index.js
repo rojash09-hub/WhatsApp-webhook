658d6503467ec1ab332f4bcd786157de612db239
rojash09-hub <rojash09@gmail.com>
2026-05-08 09:39:54 -0500

    Index.js

--- index.js
+++ index.js
@@ -6,24 +6,33 @@ const VERIFY_TOKEN = "mi_token_123";
 
 app.use(express.json());
 
+app.get('/', (req, res) => {
+  res.status(200).send("Webhook activo");
+});
+
 app.get('/webhook', (req, res) => {
   const mode = req.query['hub.mode'];
   const token = req.query['hub.verify_token'];
   const challenge = req.query['hub.challenge'];
 
-  if (mode && token === VERIFY_TOKEN) {
-    res.status(200).send(challenge);
-  } else {
-    res.sendStatus(403);
+  if (!mode) {
+    return res.status(200).send("Webhook funcionando");
   }
+
+  if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+    return res.status(200).send(challenge);
+  }
+
+  return res.sendStatus(403);
 });
 
 app.post('/webhook', (req, res) => {
-  console.log("Mensaje recibido:");
+  console.log("Payload recibido:");
   console.log(JSON.stringify(req.body, null, 2));
+
   res.sendStatus(200);
 });
 
 app.listen(PORT, () => {
-  console.log("Servidor corriendo");
+  console.log(`Servidor corriendo en puerto ${PORT}`);
 });
